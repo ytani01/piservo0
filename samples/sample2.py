882605fdfd0f68b4e524a6a4f8cd2ce6c192af2f
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2025-07-16 05:05:31 +0900

    UPD: sample2.py

--- samples/sample2.py
+++ samples/sample2.py
@@ -5,8 +5,8 @@ from piservo0 import CalibrableServo
 PIN1 = 18
 PIN2 = 21
 
-TOTAL_ANGLE1 = 180
-TOTAL_ANGLE2 = 90
+TOTAL_ANGLE1 = 180.0
+TOTAL_ANGLE2 = 90.0
 
 LOOP_N = 10
 SLEEP_SEC = 0.06
@@ -20,18 +20,21 @@ servo2 = CalibrableServo(pi, PIN2)
 angle1 = angle2 = CalibrableServo.ANGLE_MIN
 servo1.move_angle(angle1)
 servo2.move_angle(angle2)
-time.sleep(1)
+time.sleep(2)
 
-for i in range(LOOP_N + 1):
-    print(f'{angle1}, {angle2}')
+time_start = time.time()
+print(f'{time.time() - time_start:.3f}: --- START ---')
+
+for i in range(LOOP_N):
+    angle1 += TOTAL_ANGLE1 / LOOP_N
+    angle2 += TOTAL_ANGLE2 / LOOP_N
 
     servo1.move_angle(angle1)
     servo2.move_angle(angle2)
 
-    angle1 += TOTAL_ANGLE1 / LOOP_N
-    angle2 += TOTAL_ANGLE2 / LOOP_N
-
     time.sleep(SLEEP_SEC)
+
+    print(f'{time.time() - time_start:.3f}: {angle1: >6},{angle2: >6}')
 
 servo1.off()
 servo2.off()
