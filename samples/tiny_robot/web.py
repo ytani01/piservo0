9f14199aa5cc762f4f0a30b85886abb659870e7a
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2025-07-24 19:55:15 +0900

    UPD: sample/tiny_robot/web.py: cancel command "S"

--- samples/tiny_robot/web.py
+++ samples/tiny_robot/web.py
@@ -17,10 +17,13 @@ from fastapi import FastAPI
 
 from piservo0 import MultiServo, StrControl, ThrWorker
 
+
 PINS = [17, 27, 22, 25]
 ANGLE_FACTOR = [-1, -1, 1, 1]
 
 CONF_FILE = expanduser("~/servo.json")
+
+print(f"__name__={__name__}")
 
 pi = pigpio.pi()
 mservo = MultiServo(pi, PINS, conf_file=CONF_FILE, debug=False)
@@ -30,13 +33,29 @@ thr_worker.start()
     
 app = FastAPI()
 
+
+def run_cmds(cmds: str):
+    """ run command string """
+    print(f"cmds={cmds}")
+
+    for _cmd in cmds.split():
+        print(f"_cmd={_cmd}")
+
+        parsed_cmd = str_ctrl.parse_cmd(_cmd)
+        print(f"parsed_cmd={parsed_cmd}")
+
+        thr_worker.send(parsed_cmd)
+
+
 # 以下のコードを追加する
 @app.get("/")
 async def read_root():
+    """ root """
     return {"Hello": "World"}
 
 @app.get("/cmd/{cmdline}")
 async def exec_cmd(cmdline: str):
+    """ execute commands """
     print(f"cmdline='{cmdline}'")
 
     for cmd in cmdline.split():
@@ -46,9 +65,14 @@ async def exec_cmd(cmdline: str):
             thr_worker.cmdq_clear()
             continue
  
-        parsed_cmd = str_ctrl.parse_cmd(cmd)
-        print(f"parsed_cmd={parsed_cmd}")
-        
-        thr_worker.send(parsed_cmd)
+        run_cmds(cmd)
 
     return {"cmdline": cmdline}
+
+@app.get("/stop")
+async def stop_cmd():
+    """ stop and end motion """
+    thr_worker.cmdq_clear()
+
+    _end_cmds = "0.5 cccc 0.5 cFFc"
+    run_cmds(_end_cmds)
